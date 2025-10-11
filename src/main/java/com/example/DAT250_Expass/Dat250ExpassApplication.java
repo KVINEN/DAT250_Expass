559134fa06bbde6746d80a1c87e69213168950dc
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2025-10-11 13:02:10 +0200

    first commit

---
+++ src/main/java/com/example/DAT250_Expass/Dat250ExpassApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAT250_Expass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat250ExpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat250ExpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DAT250_Expass/Dat250ExpassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAT250_Expass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat250ExpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
