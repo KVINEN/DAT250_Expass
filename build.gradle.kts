dc3ba535ec0e1bcef49f96ee4fe99f804e29de06
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2025-10-12 20:31:35 +0200

    done with jacoco :)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,23 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
-	jacoco
+    application
+    jacoco
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot"
 
+
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
+}
+
+application {
+    mainClass = "com.example.Dat250ExpassApplication"
 }
 
 repositories {
@@ -22,39 +28,30 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+jacoco {
+    toolVersion = "0.8.12"
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-    extensions.configure(JacocoTaskExtension::class) {
-        //destinationFile = layout.buildDirectory.file("jacoco/jacocoTest.exec").get().asFile
-        classDumpDir = layout.buildDirectory.dir("jacoco/classpathdumps").get().asFile
-    }
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-}
-
-jacoco {
-    toolVersion = "0.8.13"
-    reportsDirectory = layout.buildDirectory.dir("jacoco")
-}
-
 tasks.jacocoTestReport {
     reports {
-        xml.required = false
+        xml.required = true
         csv.required = false
-        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+        html.outputLocation = layout.buildDirectory.dir("reports/jacoco/test/html")
     }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.jacocoTestReport)
     violationRules {
         rule {
             limit {
@@ -75,4 +72,3 @@ tasks.jacocoTestCoverageVerification {
         }
     }
 }
-//test
