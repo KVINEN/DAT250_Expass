191e0e9d5957bc48f31cc2818320dca1b7322951
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2025-10-13 21:01:38 +0200

    started on REST API

---
+++ src/main/java/com/example/DAT250_Expass/Poll.java
@@ -0,0 +1,28 @@
+package com.example.DAT250_Expass;
+
+import org.apache.logging.log4j.CloseableThreadContext;
+
+import java.time.Instant;
+import java.util.List;
+
+public class Poll {
+
+    private Integer id;
+
+    private String question;
+
+    private Instant publishedAt;
+
+    private Instant validUntil;
+
+    private Boolean isPublic;
+
+    private List<String> votingOptios;
+
+    private User user;
+
+
+
+
+
+}
